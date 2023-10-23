d7a1223dcd5eeed5dad813576df27ea216d84ec6
staf3333 <staf3333@gmail.com>
2023-10-22 20:46:08 -0700

    Create basic todo with create and readfor basic mongo container

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+# First specify the base image
+FROM node:18
+# Then create directiory to hold app code inside the image
+# will be the working directory
--- index.js
+++ index.js
@@ -1,12 +1,54 @@
 const express = require("express");
-
 const app = express();
+const Todo = require("./models/todo");
+const mongoose = require("mongoose");
 
 // View engine setup
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.sendFile("views/index.html", { root: __dirname });
+});
+
+// get all todos from DB
+app.get("/api/todos", async (req, res) => {
+  const todos = await Todo.find({});
+  console.log(todos);
+  res.send("Requested data from Mongo");
+});
+
+app.post("/api/todos", async (req, res) => {
+  // in this post route I will take in a request and attempt to save a todo in a mongo docker container
+  const { task, priority } = req.query;
+
+  const newTodo = new Todo({
+    task: task,
+    priority: priority,
+  });
+
+  await newTodo.save();
+  console.log("saved todo!");
+  // const { task, priority } = req.body;
+  // console.log(task, priority);
+  res.send(
+    `You have saved task: ${task} with priority: ${priority} to mongoDB`
+  );
+});
+
+// logic to connect to mongo db
+// first specify the mongodb connect url (this will be the address of our mongoDB docker container)
+const dbUrl = "mongodb://127.0.0.1:27017/todoApp";
+
+mongoose.connect(dbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+//new connection logic,, before I used .then and .catch
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
 });
 
 app.listen(3000, () => {
